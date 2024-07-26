0f5b4f2bbc8af62c3ae0dae2d037f89124bce79a
paolo <paolo@foo.com>
2024-07-26 21:28:07 +0200

    Sistema non ascolta alcun evento

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,4 +1,4 @@
-public class Sistema extends CPUEventFirer implements CPUListener {
+public class Sistema extends CPUEventFirer {
 //TODO: è veramente necessario passare per gli eventi?
     public CPU cpu= new CPU(this);
     public Memory RAM= new Memory(this);
@@ -6,10 +6,6 @@ public class Sistema extends CPUEventFirer implements CPUListener {
     Registro addressBUS=new Registro(-1);
     Registro dataBUS=new Registro(-1);
 
-    public void Sistema() {
-
-       // this.cpu.addCPUListener(this);
-    }
 
     public void cpuVuoleLeggereDallaMemoria() {
 
@@ -59,31 +55,4 @@ public class Sistema extends CPUEventFirer implements CPUListener {
         return (cpu.isInDecodeOrExecute());
     }
 
-    @Override
-    public void onCPUEvent(CPUEvent event) {
-        System.out.println(" SISTEMA : ricevuto CPU Event");
-    }
-
-    @Override
-    public void onRegistroChanged(RegistroChangedEvent event) {
-
-        System.out.println(" SISTEMA : ricevuto Registro CHANGED Event");
-
-    }
-
-    @Override
-    public void onRegistroRead(RegistroReadEvent event) {
-        System.out.println(" SISTEMA : ricevuto Registro READ   Event");
-
-    }
-
-    @Override
-    public void onCpuHaFinitoCicloDiClockEvent(CpuHaFinitoCicloDiClockEvent event) {
-        // non deve fare niente
-    }
-
-    @Override
-    public void onCpuAspettaImpulsoDiClockEvent(CpuAspettaImpulsoDiClockEvent event) {
-        // non deve fare niente
-    }
 }
